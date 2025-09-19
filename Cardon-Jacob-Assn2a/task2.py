8d5436dc5795a77f206b6a6f92d0d7a90dda35b9
Jacob Cardon <j7mpmz8k@duck.com>
2025-09-18 23:06:14 -0600

    polygon calculator

--- Cardon-Jacob-Assn2a/task2.py
+++ Cardon-Jacob-Assn2a/task2.py
@@ -5,6 +5,7 @@ from math import tan, pi
 import math
 
 def area_of_polygon(sides, length):
+    """function assumes a regular polygon with equal lenghts per side"""
     return round((sides * math.pow(length,3)) / (4 * tan(pi / sides)), 5)
 
 print("Welcome to the polygon calculator")
@@ -13,24 +14,30 @@ print("Welcome to the polygon calculator")
 num_sides = []
 lengths = []
 
-#poly one
-print("For polygon one, please enter...")
-num_sides.append(float(input("\tNumber of sides: ")))
-lengths.append(float(input("\tSide length: ")))
-#poly two
-print("For polygon two, please enter...")
-num_sides.append(float(input("\tNumber of sides: ")))
-lengths.append(float(input("\tSide length: ")))
-#poly three
-print("For polygon three, please enter...")
-num_sides.append(float(input("\tNumber of sides: ")))
-lengths.append(float(input("\tSide length: ")))
-#poly four
-print("For polygon four, please enter...")
-num_sides.append(float(input("\tNumber of sides: ")))
-lengths.append(float(input("\tSide length: ")))
+#input accepts integers only for number of sides but does accept floats for side lengths
+try:
+    #poly one
+    print("For polygon one, please enter...")
+    num_sides.append(int(input("\tNumber of sides: ")))
+    lengths.append(float(input("\tSide length: ")))
+    #poly two
+    print("For polygon two, please enter...")
+    num_sides.append(int(input("\tNumber of sides: ")))
+    lengths.append(float(input("\tSide length: ")))
+    #poly three
+    print("For polygon three, please enter...")
+    num_sides.append(int(input("\tNumber of sides: ")))
+    lengths.append(float(input("\tSide length: ")))
+    #poly four
+    print("For polygon four, please enter...")
+    num_sides.append(int(input("\tNumber of sides: ")))
+    lengths.append(float(input("\tSide length: ")))
 
-#loops through each polygon and prints both lists of sides and lengths
-for poly_index in range(4):
-    poly_name = poly_index + 1
-    print(f"The area of polygon {poly_name} is: {area_of_polygon(num_sides[poly_index], lengths[poly_index])}")
+except ValueError:
+    print("\nERROR! Only whole numbers of sides per polygon are allowed.")
+
+else:
+    #loops through each polygon and prints both lists of sides and lengths
+    for poly_index in range(4):
+        poly_name = poly_index + 1
+        print(f"The area of polygon {poly_name} is: {area_of_polygon(num_sides[poly_index], lengths[poly_index])}")
