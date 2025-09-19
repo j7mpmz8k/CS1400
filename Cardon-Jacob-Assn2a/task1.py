db7aee1b151d18d68a72f232b6bda3ffb2befd44
Jacob Cardon <j7mpmz8k@duck.com>
2025-09-18 23:19:21 -0600

    error handling

--- Cardon-Jacob-Assn2a/task1.py
+++ Cardon-Jacob-Assn2a/task1.py
@@ -9,18 +9,22 @@ monthly_payment = float(input("Enter the monthly payment amount: $"))#this is th
 annual_rate = float(input("Enter the annual interest rate: %"))
 years = float(input("Enter the number of years: "))
 
-#converts percentage to decimal divided monthly
-monthly_rate = (annual_rate / 100) / 12
-#calculates total months based on on years of investment
-total_months = years * 12
-growth_rate = (1 + monthly_rate) ** total_months
-#orginary annuity formula doesn't contain '* (1 + monthly_rate)' at the end
-# annuity 'due' means payments are due at the begining of each month instead of the end...giving extra month of accural
-annuity_due_rate = ((growth_rate - 1) / monthly_rate) * (1 + monthly_rate)
+try:
+    #converts percentage to decimal divided monthly
+    monthly_rate = (annual_rate / 100) / 12
+    #calculates total months based on on years of investment
+    total_months = years * 12
+    growth_rate = (1 + monthly_rate) ** total_months
+    #orginary annuity formula doesn't contain '* (1 + monthly_rate)' at the end
+    # annuity 'due' means payments are due at the begining of each month instead of the end...giving extra month of accural
+    annuity_due_rate = ((growth_rate - 1) / monthly_rate) * (1 + monthly_rate)
 
-#seperates compound interest from initial contribution from monthly annuity contributions, then combines
-initial_future_value = investment * growth_rate
-annuitiy_future_value = monthly_payment * annuity_rate
-combined_future_value = round(initial_future_value + annuitiy_future_value, 2)
+    #seperates compound interest from initial contribution from monthly annuity contributions, then combines
+    initial_future_value = investment * growth_rate
+    annuitiy_future_value = monthly_payment * annuity_rate
+    combined_future_value = round(initial_future_value + annuitiy_future_value, 2)
 
-print(f"Your future value is: ${combined_future_value}")
+    print(f"Your future value is: ${combined_future_value}")
+
+except ZeroDivisionError:# APY of zero creates error
+    print(f"Your future value is: ${investment + (monthly_payment * years)}")
