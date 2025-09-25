9b19bfcd2bb1d4467ae98266d260a76c56c0b959
Jacob Cardon <j7mpmz8k@duck.com>
2025-09-25 15:13:30 -0600

    super hero game

--- Cardon-Jacob-Assn2b/task2.py
+++ Cardon-Jacob-Assn2b/task2.py
@@ -48,17 +48,23 @@ hero_size = int(input("...Size: "))#diameter
 hero_x_location = int(input("...X Location(0-100 left to right): "))
 hero_y_location = int(input("...Y Location(0-100 bottom to top): "))
 hero_location = hero_x_location, hero_y_location
+
 #arms
 hero_arm_length = int(input("...Arm length: "))#radius
+#attack stored as a diameter from arm length extended from edge of hero's body
 super_punch = (hero_arm_length * 2) + hero_size
+
 #legs
 hero_leg_length = -inf
+#makes sure input for leg length is greater than arm length
 while hero_leg_length <= hero_arm_length:
     print(f"Leg length must be greater than arm length: {hero_arm_length}")
     hero_leg_length = int(input("...Leg length: "))#radius
     if hero_leg_length <= hero_arm_length:
         print("Invalid input. Please try again.")
+#attack stored as a diameter from leg length extended from edge of hero's body
 ninja_kick = (hero_leg_length * 2) + hero_size
+
 #sword
 hero_sword_length = -inf
 while hero_sword_length + hero_arm_length <= hero_leg_length:
@@ -67,20 +73,26 @@ while hero_sword_length + hero_arm_length <= hero_leg_length:
     hero_sword_length = int(input("...Sword length: "))#radius
     if hero_sword_length + hero_arm_length <= hero_leg_length:
         print("Invalid input. Please try again.")
+#attack stored as a diameter from sum of arm and sword length extended from edge of hero's body
 samurai_slash = (hero_sword_length * 2) + (hero_arm_length * 2) + hero_size
+
 #voice power
 hero_voice_length = -inf
+#asumes voice power length must also include the hero radius size when rubric says must be greater than arm+sword length
 while hero_voice_length < hero_arm_length + hero_sword_length + (hero_size / 2):
     print(f"Voice Length must be greater than arm and sword length from body: {hero_arm_length + hero_sword_length + (hero_size / 2)}")
     hero_voice_length = int(input("...Voice length: "))#radius
     if hero_voice_length < hero_arm_length + hero_sword_length + (hero_size / 2):
         print("Invalid input. Please try again.")
+#creates attack as diameter extended from center of hero's body
 sonic_blast = hero_voice_length * 2
+
 print(f"\nWelcome superhero {hero_name}, please keep us all safe!\n")
 
 #villain build
 villain_name = input("For the Villain enter...\n...Name: ")
 overlap = inf
+#gets user input for villains size and location...ensures villain's body doesn't overlap with hero
 while overlap > 0:
     print("Please enter the following using values: 0-100 representing percentage of screen size")
     villain_size = int(input("...Body Size: "))#diameter
@@ -90,36 +102,44 @@ while overlap > 0:
     overlap = calculate_overlap(hero_location, hero_size, villain_location, villain_size)
     if overlap > 0:
         print("\nInvalid input. Must not overlap with hero. Please try again.")
-print(f"\nOh no! The evil villain {villain_name} has appeared!")
 
+print(f"\nOh no! The evil villain {villain_name} has appeared!")
+#instructions to user to fight villain
 input(f"Hit Enter to help {hero_name} fight {villain_name}: ")
-drawly.start(title=f"{hero_name} vs {villain_name}", dimensions=(x_pixels, y_pixels), terminal=True)
+
+
+drawly.start(title=f"{hero_name} vs {villain_name}", dimensions=(x_pixels, y_pixels),
+             terminal=True, terminal_lines=1, terminal_line_height=100)
 drawly.set_speed(9)
 
 #draws hero and villain as filled in circle
 draw_circle(hero_location, width=hero_size, fill="tan")
 draw_circle(villain_location, width=villain_size, fill="black")
 
+#availbe attack actions...sizes stored as diameters
 powers = {
     1:{"name":"super punch", "size":super_punch, "color":"blue"},
     2:{"name":"ninja kick", "size":ninja_kick, "color":"green"},
     3:{"name":"samurai slash", "size":samurai_slash, "color":"red"},
     4:{"name":"sonic blast", "size":sonic_blast, "color":"purple"}
 }
+#placeholder to find the best attack as a king of the hill method...must be least non negative overlap with villain
 best_attack = {"name":None, "overlap":inf}
 #loop for both drawing each attack circumference, and finding the best attack to use
-for power in powers:
+for power in powers:#"power"=key names, not an index
     draw_circle(hero_location, powers[power]["size"], outline_thickness=3, outline=powers[power]["color"])
     overlap = calculate_overlap(hero_location, powers[power]["size"], villain_location, villain_size)
     #finds the least non negative overlap of the attack action with the villain
     if 0 < overlap < best_attack["overlap"]:
         best_attack["name"] = powers[power]["name"]
         best_attack["overlap"] = overlap
+#checks if villain is out of range from hero's attacks
 if best_attack["name"] == None:
     defeat_message = f"Oh no! {villain_name} is too far away!"
     print(defeat_message)
     drawly.terminal_output(defeat_message)
 else:
+    # if in range, displays best attack
     attack_messaage = f"{hero_name}'s best attack is {best_attack['name']}!"
     print(attack_messaage)
     drawly.terminal_output(attack_messaage)
