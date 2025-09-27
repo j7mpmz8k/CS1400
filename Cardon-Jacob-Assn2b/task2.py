4ee3b5c024b60514eb481dae3d9d7b8e692ee20c
Jacob Cardon <j7mpmz8k@duck.com>
2025-09-26 19:08:38 -0600

    super hero game

--- Cardon-Jacob-Assn2b/task2.py
+++ Cardon-Jacob-Assn2b/task2.py
@@ -4,8 +4,8 @@
 import drawly
 from math import inf
 # window dimensions...common 16:9 ratios: 3840X2160, 2560X1440, 1920x1080, 1280x720(default), 854X480, 640x360
-x_pixels = 2560
-y_pixels = 1440
+x_pixels = 1920
+y_pixels = 1080
 
 def x_ratio(x_ratio):
     """Converts a position by percentage of screen aspect ratio to pixel coordinate (0 = left, 100 = right)"""
@@ -17,40 +17,38 @@ def draw_circle(center_location, width, outline_thickness=0, outline="black", fi
     """allows to draw either a filled cicle or an outline...\n
     center_location=(x,y)...width=diameter"""
     center_x_pos, center_y_pos = center_location
-    center_x_pos = x_ratio(center_x_pos)
-    center_y_pos = y_ratio(center_y_pos)
     if outline_thickness > 0:
         drawly.set_color(outline)
-        drawly.circle(center_x_pos, center_y_pos, x_ratio(width) / 2, outline_thickness)
+        drawly.circle(center_x_pos, center_y_pos, width / 2, outline_thickness)
     else:
         drawly.set_color(fill)
-        drawly.circle(center_x_pos, center_y_pos, x_ratio(width) / 2)
+        drawly.circle(center_x_pos, center_y_pos, width / 2)
     drawly.draw()
 def distance(point1, point2):
     """calculates the pixel count distance between two points...\n
     point=(x,y)"""
     x1, y1 = point1
     x2, y2 = point2
-    return ((x_ratio(x2) - x_ratio(x1))**2 + (y_ratio(y2) - y_ratio(y1))**2)**0.5
+    return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 def calculate_overlap(point1, size1, point2, size2):
     """checks how many pixels two circles overlap...\n
     point=(x,y)...size=diameter"""
     radius1 = size1 / 2
     radius2 = size2 / 2
-    return (x_ratio(radius1) + x_ratio(radius2)) - distance(point1, point2)
+    return (radius1 + radius2) - distance(point1, point2)
 
 
 print("\nWelcome to Superhero Decisions")
 print("For size and coordinate values, please enter 0-100 representing percentage of screen size")
 # hero build
 hero_name = input("For the Superhero enter...\n...Name: ")
-hero_size = int(input("...Size: "))#diameter
-hero_x_location = int(input("...X Location(0-100 left to right): "))
-hero_y_location = int(input("...Y Location(0-100 bottom to top): "))
+hero_size = x_ratio(int(input("...Size: ")))#diameter
+hero_x_location = x_ratio(int(input("...X Location(0-100 left to right): ")))
+hero_y_location = y_ratio(int(input("...Y Location(0-100 bottom to top): ")))
 hero_location = hero_x_location, hero_y_location
 
 #arms
-hero_arm_length = int(input("...Arm length: "))#radius
+hero_arm_length = x_ratio(int(input("...Arm length: ")))#radius
 #attack stored as a diameter from arm length extended from edge of hero's body
 super_punch = (hero_arm_length * 2) + hero_size
 
@@ -59,7 +57,7 @@ hero_leg_length = -inf
 #makes sure input for leg length is greater than arm length
 while hero_leg_length <= hero_arm_length:
     print(f"Leg length must be greater than arm length: {hero_arm_length}")
-    hero_leg_length = int(input("...Leg length: "))#radius
+    hero_leg_length = x_ratio(int(input("...Leg length: ")))#radius
     if hero_leg_length <= hero_arm_length:
         print("Invalid input. Please try again.")
 #attack stored as a diameter from leg length extended from edge of hero's body
@@ -70,7 +68,7 @@ hero_sword_length = -inf
 while hero_sword_length + hero_arm_length <= hero_leg_length:
     print("Sword + arm Length must be greater than leg length:")
     print(f"Sword length must be greater than: {hero_leg_length-hero_arm_length}")
-    hero_sword_length = int(input("...Sword length: "))#radius
+    hero_sword_length = x_ratio(int(input("...Sword length: ")))#radius
     if hero_sword_length + hero_arm_length <= hero_leg_length:
         print("Invalid input. Please try again.")
 #attack stored as a diameter from sum of arm and sword length extended from edge of hero's body
@@ -81,7 +79,7 @@ hero_voice_length = -inf
 #must be greater than arm+sword length emenating from center of body
 while hero_voice_length < hero_arm_length + hero_sword_length:
     print(f"Voice Length must be greater than arm and sword length: {hero_arm_length + hero_sword_length}")
-    hero_voice_length = int(input("...Voice length: "))#radius
+    hero_voice_length = x_ratio(int(input("...Voice length: ")))#radius
     if hero_voice_length < hero_arm_length + hero_sword_length:
         print("Invalid input. Please try again.")
 #creates attack as diameter extended from center of hero's body
@@ -95,9 +93,9 @@ overlap = inf
 #gets user input for villains size and location...ensures villain's body doesn't overlap with hero
 while overlap > 0:
     print("Please enter the following using values: 0-100 representing percentage of screen size")
-    villain_size = int(input("...Body Size: "))#diameter
-    villain_x_location = int(input("...X Location(0-100 left to right): "))
-    villain_y_location = int(input("...Y Location(0-100 left to right): "))
+    villain_size = x_ratio(int(input("...Body Size: ")))#diameter
+    villain_x_location = x_ratio(int(input("...X Location(0-100 left to right): ")))
+    villain_y_location = y_ratio(int(input("...Y Location(0-100 bottom to top): ")))
     villain_location = villain_x_location, villain_y_location
     overlap = calculate_overlap(hero_location, hero_size, villain_location, villain_size)
     if overlap > 0:
@@ -108,8 +106,7 @@ print(f"\nOh no! The evil villain {villain_name} has appeared!")
 input(f"Hit Enter to help {hero_name} fight {villain_name}: ")
 
 
-drawly.start(title=f"{hero_name} vs {villain_name}", dimensions=(x_pixels, y_pixels),
-             terminal=True, terminal_lines=1, terminal_line_height=100)
+drawly.start(title=f"{hero_name} vs {villain_name}", dimensions=(x_pixels, y_pixels))
 drawly.set_speed(9)
 
 #draws hero and villain as filled in circle
@@ -135,13 +132,9 @@ for power in powers:#"power"=key names, not an index
         best_attack["overlap"] = overlap
 #checks if villain is out of range from hero's attacks
 if best_attack["name"] == None:
-    defeat_message = f"Oh no! {villain_name} is too far away!"
-    print(defeat_message)
-    drawly.terminal_output(defeat_message)
+    print(f"Oh no! {villain_name} is too far away!")
 else:
     # if in range, displays best attack
-    attack_messaage = f"{hero_name}'s best attack is {best_attack['name']}!"
-    print(attack_messaage)
-    drawly.terminal_output(attack_messaage)
+    print(f"{hero_name}'s best attack is {best_attack['name']}!")
 
 drawly.done()
---
+++ Class-Assn/dartboard.py
@@ -0,0 +1,53 @@
+# Jacob Cardon
+# CS1400 - MWF - 8:30am
+import drawly
+from random import randrange
+from math import cos, sin, radians
+#window dimensions...common 16:9 ratios: 3840X2160, 2560X1440, 1920x1080, 1280x720(default), 854X480, 640x360
+x_pixels = 1920
+y_pixels = 1080
+
+def x_ratio(x_ratio):
+    """Converts a position by percentage to pixel coordinate (0 = left, 100 = right)"""
+    return x_pixels * x_ratio / 100
+def y_ratio(y_ratio):
+    """Converts a position by percentage to pixel coordinate (0 = bottom, 100 = top)"""
+    return y_pixels * (100 - y_ratio) / 100
+
+def draw_circle(center_location, width, outline_thickness=0, outline="black", fill="white"):
+    """allows to draw either a filled cicle or an outline...\n
+    center_location=(x,y)...width=diameter"""
+    center_x_pos, center_y_pos = center_location
+    if outline_thickness > 0:
+        drawly.set_color(outline)
+        drawly.circle(center_x_pos, center_y_pos, width / 2, outline_thickness)
+    else:
+        drawly.set_color(fill)
+        drawly.circle(center_x_pos, center_y_pos, width / 2)
+    drawly.draw()
+
+target_x = x_ratio(int(input("Enter the X location for the target: ")))
+target_y = y_ratio(int(input("Enter the Y location for the target: ")))
+target_location = target_x, target_y
+target_size = x_ratio(int(input("Enter the size of the target: ")))
+
+dart_size = x_ratio(5)
+dart_offset_distance = (target_size / 2) + (dart_size / 2)
+num_of_darts = int(input("How many darts would you like? "))
+
+drawly.start(title="dart board", background="white", dimensions=(x_pixels, y_pixels), terminal=True, terminal_lines=3, terminal_line_height=50)
+drawly.set_speed(9)
+
+draw_circle(target_location, target_size, fill="blue")
+draw_circle(target_location, target_size*.66, fill="yellow")
+draw_circle(target_location, target_size*.33, fill="red")
+
+for dart_num in range(1,num_of_darts+1):
+    drawly.terminal_input(f"Press enter to throw dart #{dart_num}: ")
+    dart_rotation = randrange(360)
+    dart_offset_location = (target_x + dart_offset_distance * cos(radians(dart_rotation)),
+                            target_y + dart_offset_distance * sin(radians(dart_rotation)))
+    draw_circle(dart_offset_location, dart_size, fill="purple")
+drawly.terminal_output("Try again next time!")
+
+drawly.done()
