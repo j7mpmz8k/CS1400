4543a84bd4c401b43bdd561f5ad10bc553903a2e
Jacob Cardon <j7mpmz8k@duck.com>
2025-10-23 17:31:25 -0600

    chessboard

--- Cardon-Jacob-Assn4b/chessboard.py
+++ Cardon-Jacob-Assn4b/chessboard.py
@@ -6,13 +6,13 @@ import drawly
 def draw_chessboard(start_x:int, start_y:int, width:int=250, height:int=250):
     drawly.start(title="chessmate", dimensions=(1280, 720), background="white")
     drawly.set_speed(10)
-    draw_rectangle(start_x, start_y, width, height, boarder=True)
+    draw_rectangle(start_x, start_y, width, height, border=True)
     draw_all_rectangles(start_x, start_y, width, height)
     drawly.done()
 
 def draw_all_rectangles(start_x:int, start_y:int, width:int, height:int):
-    width = width // 8
-    height = height // 8
+    width = width / 8
+    height = height / 8
     black_tile = True
     for row in range(8):
         for col in range(8):
@@ -20,12 +20,12 @@ def draw_all_rectangles(start_x:int, start_y:int, width:int, height:int):
             black_tile = not black_tile
         black_tile = not black_tile
 
-def draw_rectangle(start_x:int, start_y:int, width:int, height:int, boarder:bool=False):
+def draw_rectangle(start_x:int, start_y:int, width:int|float, height:int|float, border:bool=False):
     drawly.set_color("black")
-    drawly.polygon_begin(stroke=1 if boarder else 0)
-    points = [(0,0),(1,0),(1,1),(0,1)]
-    for point in points:
-        x, y = point
+    drawly.polygon_begin(stroke=1) if border else drawly.polygon_begin()
+    #sets up each of the 4 points for black tiles
+    for point_modifier in [(0,0),(1,0),(1,1),(0,1)]:
+        x, y = point_modifier
         drawly.add_poly_point(start_x + x * width, start_y + y * height)
     drawly.polygon_end()
     drawly.draw()
